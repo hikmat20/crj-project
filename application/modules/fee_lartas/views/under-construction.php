480d39d9afdfda5f1b4a7e97cb212f2286e95d79
hikmat20 <hikmataulia20@hmail.com>
2023-06-21 21:04:51 +0700

    add price reverences

---
+++ application/modules/fee_lartas/views/under-construction.php
@@ -0,0 +1,14 @@
+<div class="br-pagetitle">
+    <i class="tx-primary fa-4x <?= $template['page_icon']; ?>"></i>
+    <div>
+        <h4><?= $template['title']; ?></h4>
+        <p class="mg-b-0">Lorem ipsum dolor sit.</p>
+    </div>
+</div>
+
+<div class="br-pagebody pd-x-20 pd-sm-x-30 mg-y-3 pd-t-20">
+    <div class="text-center">
+        <h1 class="tx-dark tx-bold tx-spacing-4" style="font-family: monospace;"><?= strtoupper('Under Construction...!!!'); ?></h1>
+        <img src="<?= base_url('assets/programmer.gif'); ?>" width="30%" alt="Programmer" class="rounded-10 shadow-sm">
+    </div>
+</div>
---
+++ application/modules/trucking_containers/views/form.php
@@ -0,0 +1,95 @@
+<div class="card-body">
+	<div class="form-group row">
+		<div class="col-md-4">
+			<label for="id" class="tx-dark tx-bold">ID Number<span class="tx-danger">*</span></label>
+		</div>
+		<div class="col-md-7">
+			<input type="text" readonly class="form-control" id="id" name="id" value="<?= (isset($port)) ? $port->id : null; ?>" maxlength="16" placeholder="Auto">
+		</div>
+	</div>
+	<div class="form-group row">
+		<div class="col-md-4 tx-dark tx-bold">
+			<label for="country_id">Country <span class="tx-danger">*</span></label>
+		</div>
+		<div class="col-md-7">
+			<div id="slWrapperCountry" class="parsley-select">
+				<select id="country_id" name="country_id" class="form-control select" required data-parsley-inputs data-parsley-class-handler="#slWrapperCountry" data-parsley-errors-container="#slErrorContainerCountry">
+					<option value=""></option>
+					<?php if ($countries) foreach ($countries as $country) : ?>
+						<option value="<?= $country->id; ?>" <?= (isset($port->country_id) && $port->country_id == $country->id) ? 'selected' : ''; ?>><?= $country->country_code . " - " . $country->name; ?></option>
+					<?php endforeach; ?>
+				</select>
+			</div>
+			<div id="slErrorContainerCountry"></div>
+		</div>
+	</div>
+	<div class="form-group row">
+		<div class="col-md-4">
+			<label for="city_name" class="tx-dark tx-bold">City<span class="tx-danger">*</span></label>
+		</div>
+		<div class="col-md-7">
+			<input type="text" required class="form-control" id="city_name" name="city_name" value="<?= (isset($port)) ? $port->city_name : null; ?>" placeholder="Exp: Jakarta">
+		</div>
+	</div>
+	<div class="form-group row">
+		<div class="col-md-4">
+			<label for="description" class="tx-dark tx-bold">Description</label>
+		</div>
+		<div class="col-md-7">
+			<textarea type="text" class="form-control" id="description" name="description" placeholder="Description"><?= (isset($port) && $port->description) ? $port->description : null; ?></textarea>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('.select').select2({
+			placeholder: 'Choose one',
+			dropdownParent: $('#dialog-popup'),
+			width: "100%",
+			allowClear: true
+		});
+
+		$('.select.not-search').select2({
+			minimumResultsForSearch: -1,
+			placeholder: 'Choose one',
+			dropdownParent: $('#dialog-popup'),
+			width: "100%",
+			allowClear: true
+		});
+
+		$(document).on('change', '#country_id', function() {
+			let country_id = $('#country_id').val();
+			$('#city_id').val('null').trigger('change')
+			$('#city_id').select2({
+				ajax: {
+					url: siteurl + thisController + 'getCities',
+					dataType: 'JSON',
+					type: 'GET',
+					delay: 100,
+					data: function(params) {
+						return {
+							q: params.term, // search term
+							country_id: country_id, // search term
+						};
+					},
+					processResults: function(res) {
+						return {
+							results: $.map(res, function(item) {
+								return {
+									id: item.id,
+									text: item.name
+								}
+							})
+						};
+					}
+				},
+				cache: true,
+				placeholder: 'Choose one',
+				dropdownParent: $('#dialog-popup'),
+				width: "100%",
+				allowClear: true
+			})
+		})
+	});
+</script>

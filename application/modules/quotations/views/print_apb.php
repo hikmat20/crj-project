6929127d36e59d0e6ecbeb72992b9e03c19f2b18
hikmat20 <hikmataulia20@hmail.com>
2023-08-25 10:48:55 +0700

    update print_apb quotations

--- application/modules/quotations/views/print_apb.php
+++ application/modules/quotations/views/print_apb.php
@@ -160,20 +160,20 @@
         <thead>
             <tr style="background-color:lightgray">
                 <th class="text-center align-middle" rowspan="2">No. <span class="fontA">序号</span></th>
-                <th class="text-center align-middle" rowspan="2">Items <span style="font-family: sun-exta">货物品名</span></th>
+                <th class="text-center align-middle" rowspan="2">Items<br><span style="font-family: sun-exta">货物品名</span></th>
                 <th class="text-center align-middle" rowspan="2">Specification</th>
-                <th class="text-center align-middle" rowspan="2">HS Code <span style="font-family: sun-exta">海关编码</span></th>
+                <th class="text-center align-middle" rowspan="2">HS Code<br><span style="font-family: sun-exta">海关编码</span></th>
                 <th class="text-center align-middle" rowspan="2">Add Doc.</th>
                 <th class="text-center align-middle" rowspan="2">BM without form E</th>
                 <th class="text-center align-middle" rowspan="2">BM with form E</th>
                 <th class="text-center align-middle" rowspan="2">PPH</th>
-                <th class="text-center align-middle" colspan="3">AMOUNT <span style="font-family: sun-exta">总价</span></th>
+                <th class="text-center align-middle" colspan="6">AMOUNT <span style="font-family: sun-exta">总价</span></th>
                 <th class="text-center align-middle" rowspan="2">Remark</th>
             </tr>
             <tr style="background-color:lightgray">
-                <th class="text-center border border-top-0 border-right-0">Price (<?= ($header->price_type == 'FOB') ? 'FOB' : 'CFR/CIF'; ?>)</th>
-                <th class="text-center align-middle">BM <span style="font-family: sun-exta">进口税</span></th>
-                <th class="text-center align-middle">PPh <span style="font-family: sun-exta">预付税</span></th>
+                <th colspan="2" class="text-center border border-top-0 border-right-0">Price (<?= ($header->price_type == 'FOB') ? 'FOB' : 'CFR/CIF'; ?>)</th>
+                <th colspan="2" class="text-center align-middle">BM<br><span style="font-family: sun-exta">进口税</span></th>
+                <th colspan="2" class="text-center align-middle">PPh<br><span style="font-family: sun-exta">预付税</span></th>
             </tr>
         </thead>
         <tbody>
@@ -225,21 +225,29 @@
                     <td class="text-center"><?= ($ArrHscode[$dt->origin_hscode]->bm_mfn) ?: 0; ?>%</td>
                     <td class="text-center"><?= ($ArrHscode[$dt->origin_hscode]->bm_e) ?: 0; ?>%</td>
                     <td class="text-center"><?= ($ArrHscode[$dt->origin_hscode]->pph_api) ?: 0; ?>%</td>
-                    <td class="text-right"><?= $currSymbol; ?> <?= ($dt->price) ? number_format($dt->price) : '0' ?></td>
-                    <td class="text-right"><?= $currSymbol; ?> <?= ($totalBM) ? number_format($totalBM) : '0' ?></td>
-                    <td class="text-right"><?= $currSymbol; ?> <?= ($totalPPH) ? number_format($totalPPH)  : '0' ?></td>
+                    <td style="border-right:none"><?= $currSymbol; ?></td>
+                    <td class="text-right"><?= ($dt->price) ? number_format($dt->price) : '0' ?></td>
+                    <td style="border-right:none"><?= $currSymbol; ?></td>
+                    <td class="text-right"><?= ($totalBM) ? number_format($totalBM) : '0' ?></td>
+                    <td style="border-right:none"><?= $currSymbol; ?></td>
+                    <td class="text-right"><?= ($totalPPH) ? number_format($totalPPH)  : '0' ?></td>
                     <td><?= $dt->remarks; ?></td>
                 </tr>
                 <!-- <td class="text-center"><img src="<?= ($img) ? base_url($img) : $no_image; ?>" alt="<?= ($dt->image) ?: 'no-image'; ?>" width="50px" class="img-fluid"></td> -->
             <?php endforeach; ?>
+        </tbody>
+        <tfoot>
             <tr class="bg-light" style="background-color:lightgray">
                 <th class="text-center tx-dark font-weight-bold tx-uppercase" colspan="8">Total</th>
-                <th class="text-right tx-dark font-weight-bold" id="totalPrice"><?= $currSymbol; ?> <?= number_format(($totalPrice) ?: '0'); ?></th>
-                <th class="text-right tx-dark font-weight-bold"><?= $currSymbol; ?> <?= number_format($gtotalBM); ?></th>
-                <th class="text-right tx-dark font-weight-bold"><?= $currSymbol; ?> <?= number_format(($gtotalPPH) ?: '0'); ?></th>
+                <td style="border-right:none"><?= $currSymbol; ?></td>
+                <th class="text-right tx-dark font-weight-bold" id="totalPrice"><?= number_format(($totalPrice) ?: '0'); ?></th>
+                <td style="border-right:none"><?= $currSymbol; ?></td>
+                <th class="text-right tx-dark font-weight-bold"><?= number_format($gtotalBM); ?></th>
+                <td style="border-right:none"><?= $currSymbol; ?></td>
+                <th class="text-right tx-dark font-weight-bold"><?= number_format(($gtotalPPH) ?: '0'); ?></th>
                 <th></th>
             </tr>
-        </tbody>
+        </tfoot>
     </table>
     <br>
     <table class="" width="100%">
@@ -291,7 +299,7 @@
                 </table>
                 <br>
                 <br>
-                <h4>Note: <br><br></h4>
+                <h4>Note:</h4><br>
                 <p class="border:1px solid"><?= $header->note; ?></p>
             </td>
             <td width="50%">
